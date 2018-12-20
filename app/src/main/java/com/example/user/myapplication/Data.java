070a600f763d7a476833e0746a88f799334206b8
newjlin <cc0978095701@gmail.com>
2018-12-20 15:15:49 +0800

    first commit

---
+++ app/src/main/java/com/example/user/myapplication/Data.java
@@ -0,0 +1,12 @@
+package com.example.user.myapplication;
+
+class Data {
+    Result result;
+    class Result{
+        Results[] results;
+        class Results{
+            String Station;
+            String Destination;
+        }
+    }
+}
